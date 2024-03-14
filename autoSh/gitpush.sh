c495184342197e5811e7f9173b7cb7d4d4273708
hyw-num <gosick79@126.com>
2024-03-14 14:13:57 +0800

    2024_03_14

--- autoSh/gitpush.sh
+++ autoSh/gitpush.sh
@@ -1,11 +1,23 @@
-# variable
-commitname=\'$1\'
-remotename=$2
-
-
-
+if [ $# -eq 2 ];then
+    commitname=$1
+    remotename=$2
+    cd ~/"$remotename" || return 
+    git add .
+    git commit -m "$commitname"
+    git push "$remotename" main
+elif [ $# -eq 1 ];then
+    remotename=$1
 # order
-cd ~/$remotename
-git add .
-git commit -m "$commitname"
-git push $remotename main
+    date=$(date +%Y_%m_%d)
+    cd ~/"$remotename" || return 
+    pwd
+    echo $date
+    git add .
+    git commit -m "$date"
+    git push "$remotename" main
+else
+    echo "wrong order"
+fi
+
+
+
