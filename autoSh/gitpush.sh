1d418f1d2ce7297b0a4794bce4510ef49f0ba33c
hyw-num <gosick79@126.com>
2024-03-14 14:41:12 +0800

    2024_03_14

--- autoSh/gitpush.sh
+++ autoSh/gitpush.sh
@@ -1,20 +1,32 @@
+user=hyw-num
+pass=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
+
 if [ $# -eq 2 ];then
     commitname=$1
     remotename=$2
     cd ~/"$remotename" || return 
     git add .
     git commit -m "$commitname"
-    git push "$remotename" main
+    spawn git push "$remotename" main
+    expect "Username for 'https://github.com': "
+    send "$user\r"
+    expect "Password for 'https://$user@github.com': "
+    send "$pass\r"
+
 elif [ $# -eq 1 ];then
     remotename=$1
 # order
     date=$(date +%Y_%m_%d)
     cd ~/"$remotename" || return 
     pwd
-    echo $date
+    echo "$date"
     git add .
     git commit -m "$date"
-    git push "$remotename" main
+    spawn git push "$remotename" main
+    expect "Username for 'https://github.com': "
+    send "$user\r"
+    expect "Password for 'https://$user@github.com': "
+    send "$pass\r"
 else
     echo "wrong order"
 fi
