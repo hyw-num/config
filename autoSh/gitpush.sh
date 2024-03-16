d633b93de847811bf0c745cdc95c53389dd1a8f2
hyw-num <gosick79@126.com>
2024-03-16 15:41:45 +0800

    2024_03_16

--- autoSh/gitpush.sh
+++ autoSh/gitpush.sh
@@ -1,12 +1,12 @@
 user=hyw-num
-pass=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
+pass=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
 if [ $# -eq 2 ];then
     commitname=$1
     remotename=$2
     cd ~/"$remotename" || return 
     git add .
     git commit -m "$commitname"
-    expect ~/config/autoSh/gitpush.exp $user $pass $remotename
+    expect ~/config/autoSh/gitpush.exp "$user" "$pass" "$remotename"
 elif [ $# -eq 1 ];then
     remotename=$1
 # order
@@ -16,7 +16,7 @@ elif [ $# -eq 1 ];then
     echo "$date"
     git add .
     git commit -m "$date"
-    expect ~/config/autoSh/gitpush.exp $user $pass $remotename
+    expect ~/config/autoSh/gitpush.exp "$user" "$pass" "$remotename"
 else
     echo "wrong order"
 fi
